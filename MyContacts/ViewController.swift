6fee1d088decec3e0675bdd976a0d7607175dfdb
Jesus Quezada <maj3st1@gmail.com>
2017-06-03 08:00:57 -0500

    Initial Commit

---
+++ MyContacts/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MyContacts
+//
+//  Created by Jesus Quezada on 6/3/17.
+//  Copyright © 2017 Jesus Quezada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
